6899bc56acd0020062c06c5cf59d975eefb8cd0d
97oskar <oskarszewczyk@hotmail.com>
2019-06-01 20:37:31 +0200

    Add semaphores

--- fifo_queue.h
+++ fifo_queue.h
@@ -8,9 +8,12 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <semaphore.h>
 
+#define FIRST_SEM_NAME "first_sem"
+#define SECOND_SEM_NAME "second_sem"
 #define FIRST_BUCKET_NAME "first_bucket"
-#define SECOND_BUCKET_NAME "second_bucket"
+#define SECOND_BUCKET_NAME "second_bucket" //TO DO: use concat
 #define BUCKET_SIZE 1024
 #define MEM_CHUNK 24
 
@@ -26,6 +29,9 @@ struct Queue
         int bucket1_rear, bucket2_rear;
         size_t bucket1_size, bucket2_size;
     } header;
+
+    sem_t *sem1;
+    sem_t *sem2;
 
     void *bucket1;
     void *bucket2;
@@ -53,6 +59,19 @@ struct Queue *create_queue(const char *name)
     if (queue == (struct Queue *)(-1))
     {
         perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+
+    queue->sem1 = sem_open(FIRST_SEM_NAME, O_CREAT, 0666, 1);
+    if (queue->sem1 == SEM_FAILED)
+    {
+        perror("sem_open");
+        exit(EXIT_FAILURE);
+    }
+    queue->sem2 = sem_open(SECOND_SEM_NAME, O_CREAT, 0666, 1);
+    if (queue->sem2 == SEM_FAILED)
+    {
+        perror("sem_open");
         exit(EXIT_FAILURE);
     }
 
@@ -90,6 +109,20 @@ struct Queue *get_queue(const char *name)
     if (queue == (struct Queue *)(-1))
     {
         perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+
+    queue->sem1 = sem_open(FIRST_SEM_NAME, 0);
+    if (queue->sem1 == SEM_FAILED)
+    {
+        perror("sem_open");
+        exit(EXIT_FAILURE);
+    }
+
+    queue->sem2 = sem_open(SECOND_SEM_NAME, 0);
+    if (queue->sem2 == SEM_FAILED)
+    {
+        perror("sem_open");
         exit(EXIT_FAILURE);
     }
 
@@ -131,15 +164,34 @@ void enqueue(struct Queue *queue, void *item, size_t mem_size)
 {
     if (queue->header.bucket_to_read == 1)
     {
-        memcpy(queue->bucket2 + queue->header.bucket2_rear, item, mem_size);
-        queue->header.bucket2_rear = (queue->header.bucket2_rear + MEM_CHUNK) % queue->header.bucket_capacity;
-        queue->header.bucket2_size++;
+        if (queue->header.bucket2_size && (queue->header.bucket2_front == queue->header.bucket2_rear))
+        {
+            //SEMAPHORE IN ACTION  [is full] -> [wait for swap in dequeue]
+        }
     }
     else
     {
+        if (queue->header.bucket1_size && (queue->header.bucket1_front == queue->header.bucket1_rear))
+        {
+            //SEMAPHORE IN ACTION
+        }
+    }
+
+    if (queue->header.bucket_to_read == 1)
+    {
+        sem_wait(queue->sem2);
+        memcpy(queue->bucket2 + queue->header.bucket2_rear, item, mem_size);
+        queue->header.bucket2_rear = (queue->header.bucket2_rear + MEM_CHUNK) % queue->header.bucket_capacity;
+        queue->header.bucket2_size++;
+        sem_post(queue->sem2);
+    }
+    else
+    {
+        sem_wait(queue->sem1);
         memcpy(queue->bucket1 + queue->header.bucket1_rear, item, mem_size);
         queue->header.bucket1_rear = (queue->header.bucket1_rear + MEM_CHUNK) % queue->header.bucket_capacity;
         queue->header.bucket1_size++;
+        sem_post(queue->sem1);
     }
 }
 
@@ -160,15 +212,19 @@ void *dequeue(struct Queue *queue)
 
     if (queue->header.bucket_to_read == 1)
     {
+        sem_wait(queue->sem1);
         ptr = queue->bucket1 + queue->header.bucket1_front;
         queue->header.bucket1_front = (queue->header.bucket1_front + MEM_CHUNK) % queue->header.bucket_capacity;
         queue->header.bucket1_size--;
+        sem_post(queue->sem1);
     }
     else
     {
+        sem_wait(queue->sem2);
         ptr = queue->bucket2 + queue->header.bucket2_front;
         queue->header.bucket2_front = (queue->header.bucket2_front + MEM_CHUNK) % queue->header.bucket_capacity;
         queue->header.bucket2_size--;
+        sem_post(queue->sem2);
     }
 
     return ptr;
@@ -185,6 +241,18 @@ void rm_queue(const char *name)
     if (shm_unlink(SECOND_BUCKET_NAME) == -1)
     {
         perror("shm_unlink");
+        exit(EXIT_FAILURE);
+    }
+
+    if (sem_unlink(FIRST_SEM_NAME) == -1)
+    {
+        perror("sem_close");
+        exit(EXIT_FAILURE);
+    }
+
+    if (sem_unlink(SECOND_SEM_NAME) == -1)
+    {
+        perror("sem_close");
         exit(EXIT_FAILURE);
     }
 
